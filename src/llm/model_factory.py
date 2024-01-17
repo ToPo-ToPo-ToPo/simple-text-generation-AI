0d32ba1fffe1e736a427e4e3a5e24eb3843f40e4
S.Ogawa <ogawa@struct.t.u-tokyo.ac.jp>
2024-01-17 15:33:42 +0900

    プログラムの整理を行なった。

--- src/llm/model_factory.py
+++ src/llm/model_factory.py
@@ -1,5 +1,5 @@
 
-from llm.rinna.rinna_base import RinnaPpo3_6b, RinnaSft3_6b, RinnaSftV2_3_6b
+from llm.rinna.rinna_gpt_neox_3b import RinnaGptNeox3b
 from llm.line.line_base import LineSft3_6b
 from llm.cyberagent.calm2_7b import Calm2_7b_Chat
 from llm.aituber_mal.aituber_mal_base import AituberMalBase
@@ -20,24 +20,9 @@ class ModelFactory:
     def create(self, name, processor, load_bit_size, load_in_8bit=False, load_in_4bit=False, llm_int8_enable_fp32_cpu_offload=False):
         
         # 入力されたmodel名からLLMを作成する
-        if name == "rinna/japanese-gpt-neox-3.6b-instruction-ppo":
-            return RinnaPpo3_6b(
-                processor=processor, 
-                load_bit_size=load_bit_size, 
-                load_in_8bit=load_in_8bit, 
-                load_in_4bit=load_in_4bit
-            )
-        
-        elif name == "rinna/japanese-gpt-neox-3.6b-instruction-sft-v2":
-            return RinnaSftV2_3_6b(
-                processor=processor, 
-                load_bit_size=load_bit_size, 
-                load_in_8bit=load_in_8bit, 
-                load_in_4bit=load_in_4bit
-            )
-        
-        elif name == "rinna/japanese-gpt-neox-3.6b-instruction-sft":
-            return RinnaSft3_6b(
+        if name == "rinna/japanese-gpt-neox-3.6b-instruction-ppo" or name == "rinna/japanese-gpt-neox-3.6b-instruction-sft-v2" or "rinna/japanese-gpt-neox-3.6b-instruction-sft":
+            return RinnaGptNeox3b(
+                model_name=name,
                 processor=processor, 
                 load_bit_size=load_bit_size, 
                 load_in_8bit=load_in_8bit, 
